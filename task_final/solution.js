2b0dce4762347c4a88b64f5f3c2c013b79f5c1fb
Yaroslav Trukhachev <yarik1981@mail.ru>
2021-12-02 19:34:02 +0300

    Итоговое задание. Решение на проверку.

--- task_final/solution.js
+++ task_final/solution.js
@@ -36,22 +36,19 @@
 function sendRequest(name, phone, address, goods, sum) {
 
     let data = {
-        client: name + ` ` + phone,
+	    client: name + ` ` + phone,
         order: {
             address: `ул. ` + address.street + `, дом ` + address.house + `, ` + address.entrance + ` подъезд, ` + address.floor + ` этаж, кв ` + address.flat,
             sum: sum
         }, 
         goods: [
         ]    
-    };
+	};
       
-    //let countOfGoods = goods.length;
     for (let i = 0; i < goods.length; i++) {
-       // data.goods.push(goods[i].title);
-       // data.goods.push(goods[i].count);
-       data.goods.push(goods[i].title,goods[i].count);
+       data.goods.push({title: goods[i].title,count: goods[i].count});
     };
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
     return jsonData;
 }
